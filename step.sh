c1d0a933f65ae00f8096e3e7ef5abea4eeddc6f6
kozimon0204 <monkozi0204@gmail.com>
2023-01-27 17:29:02 +0900

    init

---
+++ step.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# fail if any commands fails
+set -e
+
+# Exists if no provided parameters.
+if [ -z "${pbxproj_path}" ]; then
+  echo "[ERROR] Missing required input: pbxproj_path"
+  exit 1
+fi
+
+if [ -z "${new_icon_name}" ]; then
+  echo "[ERROR] Missing required input: new_icon_name"
+  exit 1
+fi
+
+# Exits if project file does not exists
+PBXPROJ_FILE="${pbxproj_path}"
+if [ ! -f $PBXPROJ_FILE ]; then
+    echo "[ERROR] Project file not found: ${pbxproj_path}"
+    exit 1
+fi
+
+# Info statements
+echo "[INFO] Path of .xcodeproj file:		${pbxproj_path}"
+echo "[INFO] New Icon: 		${new_icon_name}"
+
+# Replaces the setting ASSETCATALOG_COMPILER_APPICON_NAME in the .pbxproj file.
+echo ""
+echo "Replacing..."
+sed -i "" "s/\(ASSETCATALOG_COMPILER_APPICON_NAME = \).*\(;\)/\1${new_icon_name}\2/" $PBXPROJ_FILE
+echo ""
+echo "[SUCCESS] Replace done!"
